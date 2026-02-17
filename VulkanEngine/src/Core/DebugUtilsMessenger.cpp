a61af974ebf0532bc7efca5aef8d60d87839b365
012MykA <1deadeye1@mail.ru>
2026-02-17 17:38:06 +0700

    updated logging indent in DebugUtilsMessenger.cpp

--- VulkanEngine/src/Core/DebugUtilsMessenger.cpp
+++ VulkanEngine/src/Core/DebugUtilsMessenger.cpp
@@ -16,7 +16,7 @@ namespace VE
         {
             if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
             {
-                std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+                std::cerr << "validation layer: " << pCallbackData->pMessage << "\n\n";
             }
 
             return VK_FALSE;
