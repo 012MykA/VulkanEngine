8514091f29d006676e8df17525f6210fbb2c66e4
012MykA <aleshin.tomsk2@mail.ru>
2026-03-30 13:13:35 +0700

    Timer

---
+++ VulkanEngine/src/VulkanEngine/Core/Timer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <chrono>
+
+namespace ve
+{
+    class Timer
+    {
+    private:
+        using Clock = std::chrono::high_resolution_clock;
+
+    public:
+        Timer()
+        {
+            Reset();
+        }
+
+        void Reset()
+        {
+            m_Start = Clock::now();
+        }
+
+        double ElapsedSeconds() const
+        {
+            return std::chrono::duration<double>(Clock::now() - m_Start).count();
+        }
+
+        double ElapsedMilliseconds() const
+        {
+            return std::chrono::duration<double, std::milli>(Clock::now() - m_Start).count();
+        }
+
+    private:
+        std::chrono::time_point<Clock> m_Start;
+    };
+
+} // namespace ve
