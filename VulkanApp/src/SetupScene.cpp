eaeedb8109cb063837cebc4171555ac2648d26a9
012MykA <aleshin.tomsk2@mail.ru>
2026-05-02 11:30:00 +0700

    spot light

--- VulkanApp/src/SetupScene.cpp
+++ VulkanApp/src/SetupScene.cpp
@@ -1,6 +1,6 @@
 #include "ExampleLayer.hpp"
 
-#define MAT4_FROM_POS(pos) (glm::translate(glm::mat4(1.0f), pos))
+#define MAT4_FROM_POS(pos) (glm::translate(glm::mat4(1), pos))
 
 void ExampleLayer::BuildLightTestScene()
 {
@@ -10,32 +10,43 @@ void ExampleLayer::BuildLightTestScene()
 
     ve::gltf::Light purpleNeon{
         .name = "Puprle Neon",
-        .color = {0.9f, 0.0f, 0.8f},
-        .intensity = 5.0f,
+        .color = {0.9f, 0, 0.8f},
+        .intensity = 3,
         .type = ve::gltf::LightType::Point,
     };
+    m_Renderer->AddLight(purpleNeon, MAT4_FROM_POS(glm::vec3(-3, 1, 3.5f)));
 
     ve::gltf::Light yellowNeon = purpleNeon;
     yellowNeon.name = "Yellow Neon";
-    yellowNeon.color = {1.0f, 1.0f, 0.0f};
+    yellowNeon.color = {1, 1, 0};
+    m_Renderer->AddLight(yellowNeon, MAT4_FROM_POS(glm::vec3(0, 1, 3.5f)));
 
     ve::gltf::Light blueNeon = purpleNeon;
     blueNeon.name = "Blue Neon";
-    blueNeon.color = {0.0f, 0.7f, 1.0f};
+    blueNeon.color = {0, 0.7f, 1};
+    m_Renderer->AddLight(blueNeon, MAT4_FROM_POS(glm::vec3(3, 1, 3.5f)));
 
     ve::gltf::Light sun{
         .name = "Sun",
-        .color = glm::vec3(1.0f),
-        .intensity = 1.0f,
+        .color = {1, 0.6f, 0.3f},
+        .intensity = 4.5f,
         .type = ve::gltf::LightType::Directional,
     };
-
-    m_Renderer->AddLight(purpleNeon, MAT4_FROM_POS(glm::vec3(-3.0f, 2.0f, 0.0f)));
-    m_Renderer->AddLight(yellowNeon, MAT4_FROM_POS(glm::vec3(0.0f, 2.0f, 0.0f)));
-    m_Renderer->AddLight(blueNeon, MAT4_FROM_POS(glm::vec3(3.0f, 2.0f, 0.0f)));
-
-    glm::vec3 lightPos = {10.0f, 10.0f, 10.0f};
-    glm::vec3 center = {0.0f, 0.0f, 0.0f};
+    glm::vec3 lightPos = {20.0f, 3.5f, -15.0f};
+    glm::vec3 center = {0, 0, 0};
     glm::mat4 sunTransform = glm::inverse(glm::lookAt(lightPos, center, glm::vec3(0, 1, 0)));
     m_Renderer->AddLight(sun, sunTransform);
+
+    ve::gltf::Light spot{
+        .name = "Spot Light",
+        .color = {0.1f, 0.9f, 0.2f},
+        .intensity = 75,
+        .type = ve::gltf::LightType::Spot,
+    };
+
+    glm::mat4 spotTransform = glm::rotate(
+        glm::translate(glm::mat4(1.0f), glm::vec3(-7, 2, -0.2f)),
+        glm::radians(45.0f),
+        glm::vec3(0, 1, 0));
+    m_Renderer->AddLight(spot, spotTransform);
 }
